46480157629d0d05aecefc02ce5b0c4defa19fc2
jasmineranay <102012764+jasmineranay@users.noreply.github.com>
2022-04-18 12:36:35 -0700

    add new files

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 //getting all required elements 
-const start-btn = document.querySelector(".start-btn button");
-const info-box = document.querySelector(".info-box button");
-const quit-btn = document.querySelector(".quit-btn button");
-const restart_btn = document.querySelector(".restart-btn button");
+var startQuiz = document.querySelector(".start-btn button");
+var infoBox= document.querySelector(".info-box");
+var quitQuiz = document.querySelector(".buttons .quit");
+var continue_btn = infoBox.querySelector(".buttons .restart");
+var quiz_box = info-box.querySelector(".buttons .restart");
+
+start-btn.oneClick= ()=>{
+  infoBox.classList.add("activateInfo");
+
+}
+
+quit_btn.oneClick= ()=>{
+  info-box.classList.remove("activateInfo");
+}
+
+restart_btn.oneClick= ()=>{
+  info-box.classList.remove("activateInfo");
+  quiz_box.classList.remove("activateQuiz");
+}
+
